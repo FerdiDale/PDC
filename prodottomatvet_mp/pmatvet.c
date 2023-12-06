28c9025b3fd1497b9219f3a3f938208e07770a57
Ferdinando <fer.dalessandro@studenti.unina.it>
2023-12-06 07:24:10 +0100

    Migliorati dettagli e presi file di output per la documentazione

--- prodottomatvet_mp/pmatvet.c
+++ prodottomatvet_mp/pmatvet.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <time.h>
 
 #if __STDC_VERSION__ < 199901L
 #define restrict /* nothing */
@@ -10,15 +11,13 @@
 
 double *matxvet(int m, int n, double * restrict x, double * restrict A, double *elapsedTime);
 
-double **allocint2darray(int n, int m);
-
-void freeint2darray(double **a);
-
 void printVec(double* vec, int size);
 
-int get_num_threads();
+double randomDouble();
 
 int main(int argc, char* argv[]){
+
+    srand(time(NULL));
 
     int n, m, i, j;
     double *A;
@@ -27,21 +26,31 @@ int main(int argc, char* argv[]){
     double deltat, avgt;
 
     if (argc != 3) { //Nome file, n, m
-        perror("Numero di argomenti inseriti errato\n");
+        fprintf(stderr, "%s", "Numero di argomenti inseriti errato\n");
         return 1;
     }
 
     n = atoi(argv[1]);
     m = atoi(argv[2]);
 
+    if (n < 1 || m < 1) {
+        fprintf(stderr, "%s", "Dimensione richiesta per la matrice non positiva\n");
+        return 1;
+    }
+
+    if (n < omp_get_max_threads()) {
+        fprintf(stderr, "%s", "Sono presenti meno righe nella matrice che thread a disposizione\n");
+        return 1;
+    }
+
     A = malloc(n*m*sizeof(double));
     x = malloc(m*sizeof(double));
 
     for (j = 0; j < m; j++) {
         for (i = 0; i < n; i++) {
-            A[i*m+j] = 1;
+            A[i*m+j] = randomDouble();
         }
-        x[j] = 1;
+        x[j] = randomDouble();
     }
 
     avgt = 0;
@@ -52,13 +61,13 @@ int main(int argc, char* argv[]){
 
         avgt+=deltat;
 
-        // printVec(b, n);
-
     }
+
+    printVec(b, n);
 
     avgt/=10.0;
 
-    printf("Test con %d thread, taglia della matrice %dx%d, tempo impiegato medio %e\n", get_num_threads(), n, m, avgt); 
+    printf("Test con %d thread, taglia della matrice %dx%d, tempo impiegato medio %e\n", omp_get_max_threads(), n, m, avgt); 
     
     free(x);
     free(b);
@@ -79,11 +88,9 @@ double *matxvet(int m, int n, double * restrict x, double * restrict A, double *
     t0=time.tv_sec+(time.tv_usec/1000000.0);
 
     #pragma omp parallel for default(none) shared(m,n,A,x,b) private(i,j)
-    for (i=0; i<n; i++){
-
-        for (j=0; j<m; j++)
+    for (i=0; i<n; i++){ //La direttiva pragma omp for riguarda solo questo ciclo esterno, non il ciclo interno: i thread si divideranno i possibili valori di i
+        for (j=0; j<m; j++) //E poi cicleranno mediante j sull'intera i-esima riga della tabella: i thread si suddividono le intere righe
             b[i] += A[i*m+j]*x[j];
-
     }
 
     gettimeofday(&time, NULL);
@@ -105,12 +112,8 @@ void printVec(double* vec, int size) {
     printf("\n");
 }
 
-int get_num_threads() {
-    int num_threads;
-    #pragma omp parallel
-    {
-        #pragma omp single
-        num_threads = omp_get_num_threads();
-    }
-    return num_threads;
+double randomDouble() {
+    double div = RAND_MAX / 100;
+    return (rand() / div);
+    // return 1;
 }
