ac978102fc3a4db8ea8884f079e128f6700f561a
FerdiDale <fer.dalessandro@studenti.unina.it>
2023-12-26 18:33:23 +0100

    CORREZIONE, ALGORITMO FUNZIONANTE, aggiunto un caso d'uso

--- prodottomatmat/prodottomatmat.c
+++ prodottomatmat/prodottomatmat.c
@@ -106,7 +106,7 @@ int main(int argc, char* argv[]) {
         for (i = 0; i < totalN; i++) {
             for (j = 0; j < totalN; j++) {
                 A[i][j] = i * totalN + j;
-                B[i][j] = 1;
+                B[i][j] = totalN*totalN-(i * totalN + j)-1;
             }
         }
         // Stampa la matrice totale
@@ -153,7 +153,7 @@ int main(int argc, char* argv[]) {
 
 void prodottoMatMat(int** A, int** B, int** localA, int** broadcastA, int** localB, int** localC, int totalN, int localN, int numProcesses, int p, int myRank, int* coordinates, MPI_Comm gridRow, MPI_Comm gridCol) {
 
-    int k, iter;
+    int k, i, j, iter;
     int gridDims [2] = {p, p};
     int totalMatrixDims [2] = {totalN, totalN};
     int localMatrixDims [2] = {localN, localN};
@@ -167,44 +167,40 @@ void prodottoMatMat(int** A, int** B, int** localA, int** broadcastA, int** loca
         distributeMatrix(NULL, coordinates[0], coordinates[1], myRank, numProcesses, gridDims, totalMatrixDims, localMatrixDims, localB, gridRow, gridCol); //Distribuzione della matrice B
     }
 
-    for (iter = 0; iter < 10; iter++) { //Ripetiamo l'intero processo 10 volte per avere una misura del tempo di esecuzione piu' accurata
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_time = MPI_Wtime(); //Prendiamo il tempo di inizio, dopo aver sincronizzato i vari processi con la precedente Barrier
 
-        MPI_Barrier(MPI_COMM_WORLD);
-        start_time = MPI_Wtime(); //Prendiamo il tempo di inizio, dopo aver sincronizzato i vari processi con la precedente Barrier
+    for (k = 0; k < p; k++) { //Algoritmo principale che si ripete sulla k-esima diagonale
 
-        for (k = 0; k < p; k++) { //Algoritmo principale che si ripete sulla k-esima diagonale
+        //FASE DI BROADCAST
 
-            //FASE DI BROADCAST
-
-            if ((coordinates[0]+k)%p == coordinates[1]) { //I processi sulla k-esima diagonale principale dovranno inviare per primi il proprio blocco locale, quindi broadcastA coincide con localA
-                                                        //I processi sulla diagonale k-esima avranno la differenza tra gli indici j ed i pari a k
-                broadcastA = localA;
-            }
-            MPI_Bcast(broadcastA[0], localN*localN, MPI_INT, (coordinates[0] + k)%p, gridRow); 
-            //A e' allocata in modo che A[0] punti ad un blocco di memoria contiguo che contiene l'intera matrice
-            //Ad ogni riga, il processo sulla diagonale k-esima (con posizione row+k mod p) inviera' agli altri processi
-
-            //FASE DI MULTIPLY
-            multiply(broadcastA, localB, localC, localN);
-
-            if (k == p-1)
-                break; //L'ultimo roll non è necessario, risparmiamo tempo
-
-            //FASE DI ROLL
-            roll(localB, localN, myRank, numProcesses, p, coordinates, gridCol);
-
+        if ((coordinates[0]+k)%p == coordinates[1]) { //I processi sulla k-esima diagonale principale dovranno inviare per primi il proprio blocco locale, quindi broadcastA coincide con localA
+                                                    //I processi sulla diagonale k-esima avranno la differenza tra gli indici j ed i pari a k
+            broadcastA = localA;
         }
+        MPI_Bcast(broadcastA[0], localN*localN, MPI_INT, (coordinates[0] + k)%p, gridRow); 
+        //A e' allocata in modo che A[0] punti ad un blocco di memoria contiguo che contiene l'intera matrice
+        //Ad ogni riga, il processo sulla diagonale k-esima (con posizione row+k mod p) inviera' agli altri processi
 
+        //FASE DI MULTIPLY
+        multiply(broadcastA, localB, localC, localN);
 
-        end_time = MPI_Wtime();//Otteniamo il tempo di fine
+        if (k == p-1)
+            break; //L'ultimo roll non è necessario, risparmiamo tempo
 
-        elapsed_time = end_time - start_time;
-        MPI_Reduce(&elapsed_time, &total_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD); //Prendiamo il massimo dei tempi calcolati dai vari processi
-
-        if (myRank == 0)
-            printf("Dimensione matrice: %dx%d,\nNumero di processi: %d,\nTempo impiegato: %e\n\n\n\n", totalN, totalN, numProcesses, total_time);
+        //FASE DI ROLL
+        roll(localB, localN, myRank, numProcesses, p, coordinates, gridCol);
 
     }
+
+
+    end_time = MPI_Wtime();//Otteniamo il tempo di fine
+
+    elapsed_time = end_time - start_time;
+    MPI_Reduce(&elapsed_time, &total_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD); //Prendiamo il massimo dei tempi calcolati dai vari processi
+
+    if (myRank == 0)
+        printf("Dimensione matrice: %dx%d,\nNumero di processi: %d,\nTempo impiegato: %e\n\n\n\n", totalN, totalN, numProcesses, total_time);
 
 }
 
@@ -250,7 +246,7 @@ void roll(int** localB, int localN, int myRank, int numProcesses, int p, int* co
     MPI_Wait(&sendRequest, MPI_STATUS_IGNORE);
 
     // Copio i dati dalla matrice ricevuta a localB
-    memcpy(localB[0], recvB[0], localN * sizeof(int));
+    memcpy(localB[0], recvB[0], localN * localN * sizeof(int));
 
     // Liberare la memoria allocata per recvB
     free(recvB[0]);
